c03e6f5aeb0d46452b82a667d6decb92b3961a55
Elliot <ebossi10@stuy.edu>
2020-11-21 01:23:34 -0500

    removed comments

--- RealNumber.java
+++ RealNumber.java
@@ -12,12 +12,7 @@ public class RealNumber{
   public String toString(){
     return ""+getValue();
   }
-  //---------ONLY EDIT BELOW THIS LINE------------
 
-  /*
-  *Return true when the values are within 0.001% of eachother.
-  *Special case: if one is exactly zero, the other must be exactly zero.
-  */
   public boolean equals(RealNumber other){
     if (getValue()==0){
       return getValue() == other.getValue();
@@ -30,42 +25,24 @@ public class RealNumber{
     }
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *the sum of this and the other
-  */
   public RealNumber add(RealNumber other){
-     //other can be ANY RealNumber, including a RationalNumber
-     //or other subclasses of RealNumber (that aren't written yet)
      double x = getValue() + other.getValue();
      RealNumber i = new RealNumber(x);
      return i;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *the product of this and the other
-  */
   public RealNumber multiply(RealNumber other){
     double x = getValue() * other.getValue();
     RealNumber i = new RealNumber(x);
     return i;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this divided by the other
-  */
   public RealNumber divide(RealNumber other){
     double x = getValue() / other.getValue();
     RealNumber i = new RealNumber(x);
     return i;
   }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this minus the other
-  */
   public RealNumber subtract(RealNumber other){
     double x = getValue() - other.getValue();
     RealNumber i = new RealNumber(x);

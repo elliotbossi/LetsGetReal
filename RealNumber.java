23576da71b0bb270137d7a2f446a5190a55852c6
Elliot <ebossi10@stuy.edu>
2020-11-24 20:43:44 -0500

    extended to Number, removed equals

--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,4 @@
-public class RealNumber{
+public class RealNumber extends Number{
   private double value;
 
   public RealNumber(double v){
@@ -11,18 +11,6 @@ public class RealNumber{
 
   public String toString(){
     return ""+getValue();
-  }
-
-  public boolean equals(RealNumber other){
-    if (getValue()==0){
-      return getValue() == other.getValue();
-    }
-    else if (other.getValue()==0){
-      return getValue() == other.getValue();
-    }
-    else{
-      return (((Math.abs((getValue()-other.getValue())/other.getValue()))*100) <= 0.001);
-    }
   }
 
   public RealNumber add(RealNumber other){
